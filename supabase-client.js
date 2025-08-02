c48b20fb5d81e008a51bccf00cc4964558ed32ca
Zero-Eight730 <develop0914@gmail.com>
2025-08-02 18:29:56 +0900

    Update supabase-client.js

--- supabase-client.js
+++ supabase-client.js
@@ -1,6 +1,6 @@
 // public/supabase-client.js
 
 // ▼▼▼ あなたのSupabaseプロジェクトのURLとキーに書き換えてください ▼▼▼
-const SUPABASE_URL = 'postgresql://postgres.mdatvtapkbnjnuuppgbe:[YOUR-PASSWORD]@aws-0-ap-northeast-1.pooler.supabase.com:6543/postgres';
+const SUPABASE_URL = 'https://mdatvtapkbnjnuuppgbe.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1kYXR2dGFwa2Juam51dXBwZ2JlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTMyNzc4NTEsImV4cCI6MjA2ODg1Mzg1MX0.ee2pH3A76aPiXlohwljNtXnf5ITgiSoYOMpSvg0kcXw';
 const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
